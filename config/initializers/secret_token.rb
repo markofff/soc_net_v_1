1e40e2d72354e7fd836e31c95a16b266d7ba6b10
Marko Ferre <markoferre@gmail.com>
2013-05-08 16:22:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocNetV1::Application.config.secret_token = '28004d59ced572fc491964559358a635cc12e4de62ac3ecb832e1e3ae5424088b4285934993d041421305d5505ef0bd323b1e1a32db3af90a15ec1f25f8e0bf2'
